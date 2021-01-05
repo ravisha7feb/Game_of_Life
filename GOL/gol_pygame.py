12b9db4f4a67726ad432de5bc339823c363ac06d
ravisha7feb <64865136+ravisha7feb@users.noreply.github.com>
2021-01-05 21:30:50 +0530

    Fixed the bug
    1) The cell in (n - 1)th row and (n - 1)th column was not being appended, which was causing Index Error.  Fixed that by taking the entire line as input and removing the extra line in the game_file.txt file
    2) Grid was not being updated in the while loop because I had declared grid as a global constant. So I passed it separately in the functions instead of declaring it globally.

--- GOL/gol_pygame.py
+++ GOL/gol_pygame.py
@@ -3,58 +3,53 @@ from itertools import product
 import pygame
 from time import sleep
 
-def board(n):
+def board():
     grid = []
     for line in open("game_file.txt", "r"):
-        row = [int(cell) for cell in line[:-1].split() if cell != '']
+        row = [int(cell) for cell in line.split() if cell != '']
         grid.append(row)
     return grid
 #print(board(10))
 def neighbours(block, n):
-    for c in product(*(range(i - 1, i + 2) for i in block)):
-        if c != block and all(0 <= j < n for j in c):
-            yield c
+    neighbr_cells = [c for c in ((product(*(range(i - 1, i + 2) for i in block)))) if c != block and all(0 <= j < n for j in c)]
+    return neighbr_cells
 
-    
-def rule1(n, block):
+def rule1(n, block, grid):
     live_count = 0
-    for (i, j) in list(neighbours(block, n)):
-        if board(n)[i][j] == 1:
+    for i, j in neighbours(block, n):
+        if grid[i][j] == 1:
             live_count += 1
-    return True if live_count in {2, 3} else False
+    return (live_count in {2, 3})
 
-
-def rule2(n, block):
+def rule2(n, block, grid):
     live_count = 0
-    for i, j in list(neighbours(block, n)):
-        if board(n)[i][j] == 1:
+    for i, j in neighbours(block, n):
+        if grid[i][j] == 1:
             live_count += 1
-    return True if live_count == 3 else False
+    return (live_count == 3)
 
-
-def new_gen(n):
+def new_gen(grid):
+    n = len(grid[0])
     updated_board = []
-    for i in range(n):
-        updated_board.append([])
-    if n > 20:
-        grid = new_gen(n-1)
-    else:
-        grid = board(n)
-    for row_num in range(len(grid)):
-        for col_num in range(len(grid)):
-            if len(list(neighbours((row_num,col_num), n))) == 8:
+    for row_num in range(n):
+        row = []
+        for col_num in range(n):
 
-                if board(n)[row_num][col_num] == 1:
-                    if rule1(n, (row_num, col_num)):
-                        updated_board[row_num].append(1)
+                if grid[row_num][col_num] == 1:
+                    if rule1(n, (row_num, col_num), grid):
+                        row.append(1)
+                        
                     else:
-                        updated_board[row_num].append(0)
-                if board(n)[row_num][col_num] == 0:
-                    if rule2(n, (row_num, col_num)):
-                        updated_board[row_num].append(1)
+                        row.append(0)
+                        
+                if grid[row_num][col_num] == 0:
+                    if rule2(n, (row_num, col_num), grid):
+                        row.append(1)
+                        
                     else:
-                        updated_board[row_num].append(0)
-
+                        row.append(0)
+                       
+        updated_board.append(row)
     return updated_board
 
 def board_view(grid):
@@ -63,19 +58,6 @@ def board_view(grid):
             print(j, end= " ")
         print()
 
-#print(board_view(board(10)))
-#print(board_view(new_gen(10)))
-#continue = "Y" stop = "X" if user enters X we display output
-"""
-n = 20
-command = input("Enter X to view next gen")
-while command != "X":
-    command = input("Enter X to view next gen:")
-    n += 1
-
-output = new_gen(n)
-print(board_view(output))
-"""
 #pygame code:
 
 GREEN = (0, 153, 0)
@@ -99,27 +81,22 @@ def draw_square(x, y, color):
 
     pygame.draw.rect(screen, color, (top, left, size, size))
 
-def game_display(rows, cols):
-    grid = board(rows)
+def game_display(rows):
+    this_gen = board()
     user_choice = ''
-    while user_choice != 'x':
-        
+    #while user_choice != 'x':
+    while True:
         color = random.choice(COLORS)
         for x in range(rows):
-            for y in range(cols):
-                cell = grid[x][y]
+            for y in range(rows - 1):
+                cell = this_gen[x][y]
                 cell_color = color if cell == 1 else WHITE
                 draw_square(x, y, cell_color)
-
-        grid = new_gen(rows + 1)
-        pygame.display.flip()
-
-        sleep(0.5)
-        user_choice = input("Press x to quit and any other key to move towards the next generation: ")
         
-
-game_display(20, 20)
-
-
-
-
+        
+        this_gen = new_gen(this_gen)
+        pygame.display.flip()
+        sleep(0.5)
+        #user_choice = input("Press x to quit and any other key to move towards the next generation: ") 
+        
+game_display(30)
